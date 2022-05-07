2e109d9b0d95496aaf69b5835351c5a631cd431a
Gidragir <ilyabalter1@gmail.com>
2022-05-07 19:08:29 +0600

    1

--- fenrir/frontend/src/components/Main/Catalog/catalog.js
+++ fenrir/frontend/src/components/Main/Catalog/catalog.js
@@ -1,58 +1,41 @@
 /*eslint-disable */
 import React, { useEffect, useState } from 'react';
-import {
-  useQuery,
-  QueryClient,
-  QueryClientProvider,
-  useQueryClient
-} from 'react-query';
 import Functions from '../../../functions';
 import Pagination from './Pagination/pagination';
 import Products from './products';
 
-const queryClient = new QueryClient();
-
 export default function catalog() {
 
-  // const [loading, setLoading] = useState(true);
-  // const [products, setProducts] = useState([]);
-  // const addToBacket = React.useCallback((id) => {
-  //   console.log("Добавлен в корзину ", id)
-  // });
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const [currentPage, setPage] = useState(1);
+  const [productsLimit] = useState(5);
 
-  // const [currentPage, setPage] = useState(1);
-  // const [productsLimit] = useState(5);
-  
+  useEffect(() => {
+   Functions.sendReq('productsList/').then((res) => {
+     setProducts(res.data);
+     setLoading(false);
+    });
+  }, []); 
 
-  // if (loading == true) {
-  //   return (<div>Loading</div>)
-  // }
+  const lastIndex = currentPage * productsLimit;
+  const firstIndex = lastIndex - productsLimit;
+  const currentProducts = products.slice(firstIndex, lastIndex);
 
-  
+  const paginate = pageNumber => setPage(pageNumber);
 
-  // const lastIndex = currentPage * productsLimit;
-  // const firstIndex = lastIndex - productsLimit;
-  // const currentProducts = products.slice(firstIndex, lastIndex);
+  const addToBacket = React.useCallback((id) => {
+    console.log("Добавлен в корзину ", id)
+  });
 
-  // const paginate = pageNumber => setPage(pageNumber);
+  if (loading == true) {
+    return (<div>Loading</div>)
+  }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {/* <React.Fragment> */}
-      <ProductsData />
-        {/* <Pagination productsLimit={productsLimit} total={products.length} paginate={paginate} currentPage={currentPage} /> */}
-      {/* </React.Fragment> */}
-    </QueryClientProvider>
+    <React.Fragment>
+      <Products currentProducts={currentProducts} addToBacket={addToBacket} />
+      <Pagination productsLimit={productsLimit} total={products.length} paginate={paginate} currentPage={currentPage}/>
+    </React.Fragment>
   )
-}
-
-const ProductsData = () => {
-  const queryClient = useQueryClient();
-  const { isLoading, data, isSuccess } = useQuery('productsGet', () => {
-    Functions.sendReq('productsList/').then((res) => {
-      return res.data;
-    })
-  });
-  console.log(isLoading, res.data, isSuccess)
-  return <div></div>
 }
