dc2dab2525ab8a1681f751bcd764aa059f492e8c
Gidragir <ilyabalter1@gmail.com>
2022-05-20 16:40:58 +0600

    fenrir/manage:

--- fenrir/manage.py
+++ fenrir/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-#asdsadfeewdwdssdwdwthygfds
+#asdsadfeewdwdssdwdwthygfdswdwdfd
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fenrir.settings')
